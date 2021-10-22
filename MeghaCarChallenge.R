56596fb1206f97747bf1623f0559c6e4b834db53
selango007 <85666764+selango007@users.noreply.github.com>
2021-10-22 15:44:38 -0700

    Updated R script for Challenge 2

--- MeghaCarChallenge.R
+++ MeghaCarChallenge.R
@@ -2,5 +2,17 @@ library(dplyr) # for functions
 
 readfile <- read.csv("MechaCar_mpg.csv")
 df <- data.frame(readfile)
-lm(spoiler_angle ~ vehicle_weight,df) #create linear model
-summary(lm(qsec~hp,mtcars)) #summarize linear model
+temp = lm(spoiler_angle ~ vehicle_weight,df) #create linear model
+summary(lm(spoiler_angle~vehicle_weight,df)) #summarize linear model
+yvals <- temp$coefficients['vehicle_weight']*df$vehicle_weight +
+  temp$coefficients['(Intercept)']
+plt <- ggplot(df,aes(x=vehicle_weight,y=spoiler_angle)) #import dataset into ggplot2
+plt + geom_point() + geom_line(aes(y=yvals), color = "red") #plot scatter and linear model
+
+
+demo_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+total_summary <- demo_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI), SD=sd(PSI), .groups = 'keep') #create total summary table
+lot_summary <- demo_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI), SD=sd(PSI), .groups = 'keep') #create lot summary table
+
+                                          
